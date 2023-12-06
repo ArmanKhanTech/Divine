c53c291f223ce8f26fb98c65992d1e7d87f87829
arman <ak2341776@gmail.com>
2023-12-06 14:41:07 +0530

    82nd Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,11 @@ import 'package:divine/utilities/event_handlers/app_life_cycle_event_handler.dar
 import 'package:divine/utilities/no_thumb_scrollbar.dart';
 import 'package:divine/utilities/providers.dart';
 import 'package:divine/view_models/theme/theme_provider.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'options_firebase.dart';
 
 void main() async {
